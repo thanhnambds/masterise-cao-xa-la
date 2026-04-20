322b195cd2c8bb31abbb83a23539600b81a71fa4
Nam <nam@MacBook-Pro-cua-Nam-2.local>
2026-04-20 23:30:52 +0700

    feat: sync with new Google Script URL

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // --- FORM DATABASE INTEGRATION (GOOGLE SHEETS) ---
-    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyJG3sRkFxqmSIxQLOaFBUXYbYIQLID8NoEvdixJgKtHqChfDEeQ38V3M1JKCdxlI5F/exec';
+    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxFnycpCYaWPYp9k12I_8mjM5Izc-rMWoGNFPp8Zl28Uh9VVI1tc84N1Tuo-zotm0Zw/exec';
     const RECAPTCHA_SITE_KEY = 'YOUR_SITE_KEY'; // Cần thay bằng Site Key thật
 
     const handleFormSubmit = async (formElement) => {
